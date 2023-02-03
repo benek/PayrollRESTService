23bfc38004b0a6a6622c30530224021e65f5cf69
Javier Ramírez <xbenek@gmail.com>
2023-02-03 16:49:42 -0600

    all() method in Controller to list all orders.

--- src/main/java/payroll/OrderController.java
+++ src/main/java/payroll/OrderController.java
@@ -1,6 +1,14 @@
 package payroll;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class OrderController {
@@ -11,6 +19,16 @@ public class OrderController {
     public OrderController(OrderRepository repository, OrderModelAssembler assembler) {
         this.repository = repository;
         this.assembler = assembler;
+    }
+
+    @GetMapping("/orders")
+    public CollectionModel<EntityModel<Order>> all() {
+        List<EntityModel<Order>> orders = repository.findAll().stream()
+                .map(assembler::toModel)
+                .toList();
+
+        return CollectionModel.of(orders,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
     }
 
 }
